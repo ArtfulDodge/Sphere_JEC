648110d75dad947464c255124b421ea2198ad7f5
coffejo <coffejo@bc.edu>
2020-01-31 10:48:02 -0500

    first commit

---
+++ Sphere/SphereTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sphere
+{
+    class SphereTest
+    {
+        public static void Main(String[] args)
+        {
+            Sphere defaultSphere = new Sphere();
+            Console.WriteLine(defaultSphere.ToString());
+            Console.WriteLine(defaultSphere.GetDiameter());
+            Console.WriteLine(defaultSphere.GetSurfaceArea());
+            Sphere newSphere = new Sphere(15);
+            Console.WriteLine(newSphere.ToString());
+            Console.WriteLine(newSphere.GetDiameter());
+            Console.WriteLine(newSphere.GetSurfaceArea());
+        }
+    }
+}
